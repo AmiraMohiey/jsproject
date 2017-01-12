88239ec1ab946e28a1d228d1db07ce7e21382639
mhesham90 <mohammedhesham.1990@gmail.com>
2017-01-12 14:57:03 +0200

    js file edited

--- moving background/bg.js
+++ moving background/bg.js
@@ -1,27 +1,5 @@
-/*var canvas = document.getElementById('hcan');
-var ctx = canvas.getContext("2d");
-canvas.width = 1000;
-canvas.height = 700;
 
-//draw Image
-var velocity=100;
-var bgImage = new Image();
-bgImage.addEventListener('load',drawImage,false);
-bgImage.src = "img.jpg";
-function drawImage(time){          
-        var framegap=time-lastRepaintTime;
-		lastRepaintTime=time;
-		var translateX=velocity*(framegap/1000);
-		ctx.clearRect(0,0,canvas.width,canvas.height);
-	 	var pattern=ctx.createPattern(bgImage,"repeat-x");
-		ctx.fillStyle=pattern;
-		ctx.rect(translateX,0,bgImage.width,bgImage.height);
-	   ctx.fill();
-        ctx.translate(-translateX,0);	
-	requestAnimationFrame(drawImage);
-}
-var lastRepaintTime=window.performance.now();*/
-
+/*
 
 var x = rocket.width/2;
 var y = rocket.height/2;
@@ -76,3 +54,47 @@ function draw() {
 
 setInterval(draw, 10);
 
+*/
+var keys = [];
+var elmnt = document.getElementById("image1");
+
+function leftArrowPressed() {
+     
+            elmnt.style.left = parseInt(elmnt.style.left) - 10 + 'px';
+}
+
+function rightArrowPressed() {
+    
+            elmnt.style.left = parseInt(elmnt.style.left) + 10 + 'px';
+    
+}
+
+window.addEventListener('keydown', keysPressed, false);
+window.addEventListener('keyup', keysReleased, false);
+ 
+
+ 
+function keysPressed(e) {
+    keys[e.keyCode] = true;
+     
+    if (keys[37]) {
+        leftArrowPressed();
+    }
+     
+    if (keys[39]) {
+     
+       rightArrowPressed(); 
+    }
+}
+ 
+function keysReleased(e) {
+    keys[e.keyCode] = false;
+      if (keys[37]) {
+        leftArrowPressed();
+    }
+     
+    if (keys[39]) {
+     
+       rightArrowPressed(); 
+    }
+}
