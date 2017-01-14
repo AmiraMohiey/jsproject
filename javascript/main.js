c12ce7ff8b4d1bbe0ffeb63dc9a6cb4ef0d221d9
AmiraMohiey <ameramohiey92@gmail.com>
2017-01-14 21:57:33 +0200

    adding menu to game

--- javascript/main.js
+++ javascript/main.js
@@ -11,7 +11,7 @@ var main = {
     shipsrc: "images/r3.png",
     FINISH: false,
     interval: 50,
-    
+
     characterScreen: document.getElementById("char"),
     firstScreen: document.getElementById("start"),
     gameScreen: document.getElementById("container"),
@@ -20,29 +20,29 @@ var main = {
     btnStart: document.getElementById("letstart"),
     btnChooseCharachter: document.getElementById("play"),
     btnPlayAgain: document.getElementById("pa"),
-    
+
     score: document.getElementById("score"),
     level: document.getElementById("level"),
     astroids: [],
-    astSizes:[50,100,150],
+    astSizes: [50, 100, 150],
     lastGenerat: new Date().getTime(),
-    level_speed: parseInt(level.innerText)*4,
+    level_speed: parseInt(level.innerText) * 4,
+    mainmenu: document.getElementById("mainmenu"),
 
-
-    random : function(size) {
+    random: function(size) {
 
         return parseInt(Math.random() * size);
     },
 
-
+    menuplaybtn: document.getElementById("menuplaybtn"),
 
 
 
     init: function() {
         main.btnStart.addEventListener('click', main.clkStart);
-     
+        main.menuplaybtn.addEventListener('click', main.menuplay);
     },
-    startGame: function(){
+    startGame: function() {
 
         main.ship = new Ship(main.shipsrc, main.shipWidth, main.shipHeight, main.shipX, main.shipY, 'ship');
         window.onmousemove = function(e) {
@@ -52,7 +52,7 @@ var main = {
             if (e.clientY < 590) {
                 ship.style.top = e.clientY + 'px';
             }
-            
+
 
         }
         main.inc_score();
@@ -60,66 +60,63 @@ var main = {
     },
 
     loop: function() {
-        
+
         main.addAst();
         main.updatePosition();
-        for (var i = 0; i < main.astroids.length; i++){
-            main.detectCollision(main.ship,main.astroids[i])
+        for (var i = 0; i < main.astroids.length; i++) {
+            main.detectCollision(main.ship, main.astroids[i])
         }
-        
-        setTimeout(function(){main.loop();},40 );
+
+        setTimeout(function() { main.loop(); }, 40);
     },
     addAst: function() {
-          
-          if ( new Date().getTime()-main.lastGenerat>1000 ){   
-            
+
+        if (new Date().getTime() - main.lastGenerat > 1000) {
+
             main.lastGenerat = new Date().getTime();
             var size = main.astSizes[main.random(main.astSizes.length)]
-            var astObj = new Astroid(main.random(main.gameW),20,size,"./images/asteroid1.gif",Math.random() >= 0.5,main.lastGenerat);
+            var astObj = new Astroid(main.random(main.gameW), 20, size, "./images/asteroid1.gif", Math.random() >= 0.5, main.lastGenerat);
             main.astroids.push(astObj);
-            
-            }
-    },
-    updatePosition: function(){
 
-       
-        for (var i = 0; i < main.astroids.length; i++)
-        {
-            
-           console.log(i,main.astroids.length)
-            if(main.astroids[i].out)
-                {
-               
+        }
+    },
+    updatePosition: function() {
+
+
+        for (var i = 0; i < main.astroids.length; i++) {
+
+            console.log(i, main.astroids.length)
+            if (main.astroids[i].out) {
+
                 main.astroids[i].kill();
-                
+
                 main.astroids.splice(i, 1);
-                }            
-            else{
-                if(main.astroids[i].move)
-                    main.astroids[i].x +=main.level_speed;
-                main.astroids[i].y +=main.level_speed ;            
+            } else {
+                if (main.astroids[i].move)
+                    main.astroids[i].x += main.level_speed;
+                main.astroids[i].y += main.level_speed;
             }
         }
 
     },
-    detectCollision: function (obj1,obj2){
-         
-            console.log("detectcol");
-            var obj1x=parseInt(obj1.element.style.left);
-            var obj1y=parseInt(obj1.element.style.top);
-            var obj1w=parseInt(obj1.element.style.width);
-            var obj1h=parseInt(obj1.element.style.height);
-            var obj2x=parseInt(obj2.x);
-            var obj2y=parseInt(obj2.y);
-            var obj2w=parseInt(obj2.w);
-            var obj2h=parseInt(obj2.w);
-            if (obj1x < obj2x + obj2w &&
+    detectCollision: function(obj1, obj2) {
+
+        console.log("detectcol");
+        var obj1x = parseInt(obj1.element.style.left);
+        var obj1y = parseInt(obj1.element.style.top);
+        var obj1w = parseInt(obj1.element.style.width);
+        var obj1h = parseInt(obj1.element.style.height);
+        var obj2x = parseInt(obj2.x);
+        var obj2y = parseInt(obj2.y);
+        var obj2w = parseInt(obj2.w);
+        var obj2h = parseInt(obj2.w);
+        if (obj1x < obj2x + obj2w &&
             obj1x + obj1w > obj2x &&
             obj1y < obj2y + obj2h &&
             obj1h + obj1y > obj2y) {
-                main.endGame();
-            }
-        
+            main.endGame();
+        }
+
     },
 
 
@@ -130,12 +127,25 @@ var main = {
         main.btnChooseCharachter.addEventListener('click', main.chooseCharacter);
         main.firstScreen.className = "hidden";
         main.gameScreen.className = "hidden";
-        main.characterScreen.className = "char";
-        main.gameOverScreen.className="hidden";
+        main.characterScreen.className = "hidden";
+        main.gameOverScreen.className = "hidden";
+        main.mainmenu.className = "menu2";
     },
 
 
+    menuplay: function() {
 
+
+        main.startGame();
+        main.characterScreen.className = 'hidden';
+        main.firstScreen.className = "hidden";
+        main.gameScreen.className = "nocursor ";
+        main.gameOverScreen.className = "hidden";
+        main.mainmenu.className = "hidden";
+        main.ship.element.src = "images/r33.png";
+
+
+    },
 
 
     chooseCharacter: function() {
@@ -144,8 +154,8 @@ var main = {
         main.characterScreen.className = 'hidden';
         main.firstScreen.className = "hidden";
         main.gameScreen.className = "nocursor ";
-        main.gameOverScreen.className="hidden";
-
+        main.gameOverScreen.className = "hidden";
+        main.mainmenu.className = "hidden";
         var rocket1 = document.getElementById("r1").checked;
         var rocket2 = document.getElementById("r2").checked;
         var rocket3 = document.getElementById("r3").checked;
@@ -166,7 +176,7 @@ var main = {
             main.ship.element.src = "images/r.png";
 
         }
-        
+
     },
 
     inc_score: function() {
@@ -189,11 +199,11 @@ var main = {
 
         var value2 = parseInt(level.innerText) + 1;
         level.innerText = value2;
-        main.level_speed=parseInt(level.innerText)*2;
+        main.level_speed = parseInt(level.innerText) * 2;
 
     },
 
-    endGame: function(){
+    endGame: function() {
         console.log("endgame");
         main.firstScreen.className = "hidden";
         main.gameScreen.className = "hidden";
